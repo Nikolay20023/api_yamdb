db68da68fab66d5d2ac18c44a8f8d933a8d257ae
Николай Судаков <sudakovkoli@mail.ru>
2022-11-05 03:28:58 +0900

    IDasdasd

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -4,9 +4,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
-from rest_framework import filters
-from django.core.mail import send_mail
-
 
 from .serializers import RegistrationSerializer, AdminSerializer
 
